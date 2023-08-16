c537b42d9371262fd83454cbe5a49e743bea83cb
Alexander Evans <sander7989@gmail.com>
2023-08-15 22:06:37 -0600

    add scripting support for loading multiple pre-existing tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,17 +5,30 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
 const createTweetElement = function(tweetData) {
   const $tweet = $(`
@@ -37,17 +50,17 @@ const createTweetElement = function(tweetData) {
 };
 
 
+const renderTweets = function(tweetsArray) {
+  for (let tweet of tweetsArray){    
+    $('#tweet-container').append(createTweetElement(tweet));   
+  }
+};
+
 
 $(function() {
-
-  const $tweet = createTweetElement(tweetData);
-
-  console.log($tweet);
-
-  $('#tweet-container').append($tweet);
   
-  //Get existing tweets
-  
+  //Get and render existing tweets
+  renderTweets(data);
   
   //Post new tweet
   /*$(".new-tweet button").on("click", function() {
