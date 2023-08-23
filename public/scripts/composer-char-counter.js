f1e9a47d6bb35fca38d3355398b0da44ec06fa82
Alexander Evans <sander7989@gmail.com>
2023-08-22 22:44:56 -0600

    change counter colour when below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,14 @@ $(function() { // Apparently wrapping this like so is necessary?
   $("#tweet-text").on("input", function() {
     const count = 140 - this.value.length;
     
-    //this.parentNode.childNodes[5].childNodes[3].value = count; // WHY WOULD I?
     $(".counter").val(count);
     
     if (count < 0) {
       this.style.color = "red";
+      $(".counter").css("color", "red");
     } else {
       this.style.color = "black";
+      $(".counter").css("color", "black");
     }
   });
 });
